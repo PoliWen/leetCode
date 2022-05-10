4fb587b5f633abe761edd00bf2eea649b36d49d0
文孝礼 <873619879@qq.com>
2022-05-10 15:34:31 +0800

    feat: leetCode每日一题

--- 两数之和.js
+++ 两数之和.js
@@ -3,7 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
-
+const nums = [1,5,3,7,9,6,5,4]
+const target = 10
 // 第一种采用双重遍历的简单粗暴方式
 const twoSum = (nums, target) => {
     const result = []
@@ -16,30 +17,39 @@ const twoSum = (nums, target) => {
         }
     }
     return result
-};
-const nums = [1,5,3,7,9,6,5,4]
-const target = 10
+}
 console.log(twoSum(nums,target)) // [2,3]
 
 // 方法2 使用map进行反推
 const twoSum2 = (nums, target)=>{
     const numsMap = new Map()
     for(let i=0;i<nums.length;i++){
-        if(numsMap.has(target - nums[i])){
+        const x = target - nums[i]
+        if(numsMap.has(x)){
             return [numsMap.get(x),i]
         }
         numsMap.set(nums[i],i)
     }
 }
-const nums = [1,5,3,7,9,6,5,4]
-const target = 10
 console.log(twoSum2(nums,target)) // [2,3]
 
-
-
 // 方法3 采用二分法
-
-
-
-// 方法4 采用四分法
+const twoSum3 = (nums,target)=>{
+    const result = []
+    for(let i=0;i<nums.length;i++){
+        let x = target - nums[i]
+        for(let j=i+1, k=nums.length-1;j<=k;j++,k--){
+            if(nums[j]===x){
+                result.push(i)
+                result.push(j)
+            }
+            if(nums[k]===x){
+                result.push(i)
+                result.push(k)
+            }
+        }
+    }
+    return Array.from(new Set(result)) // j=k的时候会重复计算一次，所以要去重处理
+}
+console.log(twoSum3(nums,target))
 
