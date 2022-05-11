757502842b2136d4267073585bbb2c8fb863dfe8
文孝礼 <873619879@qq.com>
2022-05-11 11:38:43 +0800

    feat: 回文数算法

---
+++ 回文数判断.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+// 初级写法
+const isPalindrome = function(nums) {
+    return String(nums) === String(nums).split('').reverse().join('')
+}
+const num = 123321
+console.log(isPalindrome(num))
+
+// 加上边界判断，
+const isPalindrome2 = function(x) {
+    if(x<0 || (x%10===0 && x!=0)) return false
+    const nums = String(x)
+    // 负数和0结尾的数字不是回文数字
+    return nums === nums.split('').reverse().join('')
+}
+const num2 = -1233210
+console.log(isPalindrome2(num2))
+
+
+// 通过二分法，进行首位比较
+const isPalindrome3 = function(x) {
+    // -数和0结尾的数字不是回文数字
+    if(x<0 || (x%10===0 && x!=0)) return false
+    let reversedNumber = 0
+    while(x > reversedNumber){
+        reversedNumber = reversedNumber*10 + x % 10
+        x = Math.floor(x/10)
+    }
+    return x===reversedNumber || x=== Math.floor(reversedNumber/10)
+}
+const num3 = 12321
+console.log('333',isPalindrome3(num3))
